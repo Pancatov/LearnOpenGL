84ddbee22d2a78dea852d479db42bc14d0559979
Pancatov <albertoddczolet@gmail.com>
2023-10-07 17:23:52 -0300

    etst
    test

--- src/6.pbr/1.1.lighting - Copia/1.1.pbr.fs
+++ src/6.pbr/1.1.lighting - Copia/1.1.pbr.fs
@@ -5,11 +5,10 @@ in vec3 WorldPos;
 in vec3 Normal;
 
 // material parameters
-uniform sampler2D albedoMap;
-uniform sampler2D normalMap;
-uniform sampler2D metallicMap;
-uniform sampler2D roughnessMap;
-uniform sampler2D aoMap;
+uniform vec3 albedo;
+uniform float metallic;
+uniform float roughness;
+uniform float ao;
 
 // lights
 uniform vec3 lightPositions[4];
@@ -18,27 +17,6 @@ uniform vec3 lightColors[4];
 uniform vec3 camPos;
 
 const float PI = 3.14159265359;
-// ----------------------------------------------------------------------------
-// Easy trick to get tangent-normals to world-space to keep PBR code simplified.
-// Don't worry if you don't get what's going on; you generally want to do normal 
-// mapping the usual way for performance anyways; I do plan make a note of this 
-// technique somewhere later in the normal mapping tutorial.
-vec3 getNormalFromMap()
-{
-    vec3 tangentNormal = texture(normalMap, TexCoords).xyz * 2.0 - 1.0;
-
-    vec3 Q1  = dFdx(WorldPos);
-    vec3 Q2  = dFdy(WorldPos);
-    vec2 st1 = dFdx(TexCoords);
-    vec2 st2 = dFdy(TexCoords);
-
-    vec3 N   = normalize(Normal);
-    vec3 T  = normalize(Q1*st2.t - Q2*st1.t);
-    vec3 B  = -normalize(cross(N, T));
-    mat3 TBN = mat3(T, B, N);
-
-    return normalize(TBN * tangentNormal);
-}
 // ----------------------------------------------------------------------------
 float DistributionGGX(vec3 N, vec3 H, float roughness)
 {
@@ -53,24 +31,6 @@ float DistributionGGX(vec3 N, vec3 H, float roughness)
 
     return nom / denom;
 }
-
-//Nova função de Blinn
-float D(vec3 N, vec3 H, float roughness)
-{
-    float alpha = roughness*roughness;
-    float alpha2 = alpha*alpha;
-    
-    //CÁLCULO
-    float blinn = (1.0f/(PI * alpha2))*(pow(dot(N,H),(2.0f/alpha2)-2.0f));
-
-    return blinn;
-
-
-
-
-}
-
-
 // ----------------------------------------------------------------------------
 float GeometrySchlickGGX(float NdotV, float roughness)
 {
@@ -81,17 +41,6 @@ float GeometrySchlickGGX(float NdotV, float roughness)
     float denom = NdotV * (1.0 - k) + k;
 
     return nom / denom;
-}
-
-//Nova função de Neumann
-float G(vec3 N, vec3 V, vec3 L)
-{
- //CÁLCULO
- float neumann = (dot(N,L) * dot(N,V))/(max(dot(N,L),dot(N,V)));
-
- return neumann;
-
-
 }
 // ----------------------------------------------------------------------------
 float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness)
@@ -108,23 +57,10 @@ vec3 fresnelSchlick(float cosTheta, vec3 F0)
 {
     return F0 + (1.0 - F0) * pow(clamp(1.0 - cosTheta, 0.0, 1.0), 5.0);
 }
-
-//Nova função None
-vec3 F(vec3 F0)
-{
-return F0;
-}
-
-
 // ----------------------------------------------------------------------------
 void main()
-{
-    vec3 albedo     = pow(texture(albedoMap, TexCoords).rgb, vec3(2.2));
-    float metallic  = texture(metallicMap, TexCoords).r;
-    float roughness = texture(roughnessMap, TexCoords).r;
-    float ao        = texture(aoMap, TexCoords).r;
-
-    vec3 N = getNormalFromMap();
+{		
+    vec3 N = normalize(Normal);
     vec3 V = normalize(camPos - WorldPos);
 
     // calculate reflectance at normal incidence; if dia-electric (like plastic) use F0 
@@ -143,14 +79,10 @@ void main()
         float attenuation = 1.0 / (distance * distance);
         vec3 radiance = lightColors[i] * attenuation;
 
-         // Cook-Torrance BRDF
-        //float NDF = DistributionGGX(N, H, roughness);   
-        //float G   = GeometrySmith(N, V, L, roughness);      
-        //vec3 F    = fresnelSchlick(clamp(dot(H, V), 0.0, 1.0), F0);
-
-        float NDF = D(N, H, roughness);   
-        float G   = G(N, V, L);      
-        vec3 F    = F(F0);
+        // Cook-Torrance BRDF
+        float NDF = DistributionGGX(N, H, roughness);   
+        float G   = GeometrySmith(N, V, L, roughness);      
+        vec3 F    = fresnelSchlick(clamp(dot(H, V), 0.0, 1.0), F0);
            
         vec3 numerator    = NDF * G * F; 
         float denominator = 4.0 * max(dot(N, V), 0.0) * max(dot(N, L), 0.0) + 0.0001; // + 0.0001 to prevent divide by zero
@@ -165,7 +97,7 @@ void main()
         // multiply kD by the inverse metalness such that only non-metals 
         // have diffuse lighting, or a linear blend if partly metal (pure metals
         // have no diffuse light).
-        kD *= 1.0 - metallic;
+        kD *= 1.0 - metallic;	  
 
         // scale light by NdotL
         float NdotL = max(dot(N, L), 0.0);        
@@ -177,7 +109,7 @@ void main()
     // ambient lighting (note that the next IBL tutorial will replace 
     // this ambient lighting with environment lighting).
     vec3 ambient = vec3(0.03) * albedo * ao;
-    
+
     vec3 color = ambient + Lo;
 
     // HDR tonemapping
